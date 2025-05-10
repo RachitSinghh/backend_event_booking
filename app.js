344a9ff8dc7cbdd122b588893b4626b10f769cb7
RachitSinghh <singhrachit@outlook.in>
2025-05-10 21:29:23 +0530

    Initialize Event Booking API project with basic structure and configuration
    - Set up Express server with MongoDB connection using Mongoose
    - Configured environment variables using dotenv
    - Installed essential packages: express, mongoose, bcryptjs, jsonwebtoken, cors
    - Added base folder structure: controllers, models, routes, middlewares, config
    - Created initial app and server setup with a root health check route

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+const cors = require('cors');
+const authRoute = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+
+app.get('/', (req,res) => res.send('Event Booking API is live'));
+
+module.exports = app; 
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./app.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
